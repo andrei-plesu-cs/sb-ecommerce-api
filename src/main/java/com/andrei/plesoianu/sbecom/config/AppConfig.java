99b43f41fc5b878848b7b09113087aab5ed87851
Andrei-Bogdan PLEŞOIANU (94849) <andrei.plesoianu@stud.acs.upb.ro>
2025-09-19 16:11:09 +0300

    Added CORS config for localhost

--- src/main/java/com/andrei/plesoianu/sbecom/config/AppConfig.java
+++ src/main/java/com/andrei/plesoianu/sbecom/config/AppConfig.java
@@ -9,6 +9,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -44,5 +46,19 @@ public class AppConfig {
         });
 
         return mapper;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // allow all endpoints
+                        .allowedOrigins("http://localhost:5173") // allow your Vite frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
